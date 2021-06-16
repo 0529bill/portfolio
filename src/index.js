3463ffc6fcb54b88be636210af7f8819b5c87f6a
water <0529bill@gmail.com>
2021-06-16 15:27:43 +0800

    delete reportWebVitals

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import AOS from 'aos';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
 import './style.css';
-// AOS.init();
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/lang/i18n/__tests__/i18n.test.js
+++ src/lang/i18n/__tests__/i18n.test.js
@@ -1,14 +1,14 @@
 import i18n from '../i18n';
 
 describe('[lang] [i18n] function export correctly', () => {
+  const t = i18n();
+
   it('exports successfully', () => {
-    let t = i18n();
     expect(typeof t('frontend.test.lang.en')).toBe('string');
     expect(t('frontend.test.lang.en')).toBe('testing');
   });
 
   it('exports wrong i18n key', () => {
-    let t = i18n();
     const fakeKey = 'frontend.local.fake.keys';
     expect(typeof t('frontend.local.fake.keys')).toBe('string');
     expect(t('frontend.local.fake.keys')).toBe(
